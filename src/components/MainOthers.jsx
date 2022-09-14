e5ffdb668d3af8f9836d8d3cc8260f126759dbab
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2022-09-14 14:35:15 -0300

    Change link button depositos judiciais.

--- src/components/MainOthers.jsx
+++ src/components/MainOthers.jsx
@@ -8,7 +8,7 @@ const MainOthers = () => {
           <li> <a href="https://www.sare.pe.gov.br/contracheque/" target="_blank"> CONTRA-CHEQUE </a></li>
           <li> <a href="https://www.sadspm.pe.gov.br/web" target="_blank"> SPM </a></li>
           <li> <a href="https://www.ilovepdf.com/pt" target="_blank"> PDF's </a></li>
-          <li> <a href="https://depositojudicial.caixa.gov.br/sigsj_internet/depositos-judiciais/justica-estadual/" target="_blank"> DEPÓSITOS JUDICIAIS </a></li>
+          <li> <a href="https://www.bb.com.br/pbb/pagina-inicial/setor-publico/judiciario/deposito-judicial#/" target="_blank"> DEPÓSITOS JUDICIAIS </a></li>
           <li> <a href="https://tthiagocarlosdev.github.io/geradordesenha/" target="_blank"> GERADOR DE SENHA </a></li>
         </ul>
     </div>

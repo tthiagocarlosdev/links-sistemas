f10c8f1b50788064764c0f01b73281d8af813320
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-06-08 14:47:44 -0300

    Add link SPP

--- src/components/MainGeneral.jsx
+++ src/components/MainGeneral.jsx
@@ -18,6 +18,7 @@ import { useNavigate } from 'react-router-dom';
       <ul className="links-general">
         <li> <a href="http://www.policiacivil.pe.gov.br/" target="_blank" > SITE PCPE </a></li>
         <li> <a href="https://pcpevirtual.policiacivil.pe.gov.br/pcp/open.do?action=open&sys=PCP" target="_blank" > PCPE VIRTUAL </a></li>
+        <li> <a href="https://spp.pc.pe.gov.br/#/controle" target="_blank" > SPP </a></li>
         <li> <a href="https://sei.pe.gov.br/sip/login.php?sigla_orgao_sistema=GOVPE&sigla_sistema=SEI" target="_blank" > SEI </a></li>
         <li> <a href="https://www.expresso.pe.gov.br/login.php?cd=1" target="_blank" >  EXPRESSO </a></li>
         <li> <a href="https://drive.expresso.pe.gov.br/login" target="_blank" >  DRIVE PE </a></li>

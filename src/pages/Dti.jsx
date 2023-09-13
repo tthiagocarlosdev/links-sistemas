709a2d431c3771aea133269ee724e6e9e0b0aea9
tthiagocarlosdev <tthiagocarlos.dev@gmail.com>
2023-09-13 16:46:43 -0300

    adicionado link GLPI na pagina DTI; retirado link relatorio da pagina DTI

--- src/pages/Dti.jsx
+++ src/pages/Dti.jsx
@@ -20,11 +20,11 @@ const Dti = () => {
         <div className="box-dti">
           <h1 className="title-dti"> DTI </h1>
           <ul className="links-dti">
-              <li> <a href="http://200.238.83.219/old-PCPE/rse.htm" target="_blank" > RELATÓRIO </a></li>
-              <li> <a href="https://tthiagocarlosdev.github.io/projects_website/relatorio_de_servicos_externos/index.html" target="_blank" > RELATÓRIO NOVO </a></li>
-              <li> <a href="https://spp-treinamento.pc.pe.gov.br/#/controle" target="_blank" > SPP TREINAMENTO </a></li>
-              <li> <a href="http://200.238.83.219/old-PCPE/JAVA/" target="_blank" > JAVA </a></li>
-              <li> <a href="http://200.238.83.219/old-PCPE/AD/" target="_blank" > ANYDESK </a></li>
+            <li> <a href="https://tthiagocarlosdev.github.io/projects_website/relatorio_de_servicos_externos/index.html" target="_blank" > RELATÓRIO </a></li>
+            <li> <a href="https://glpi.pc.pe.gov.br/" target="_blank" > GLPI </a></li>
+            <li> <a href="https://spp-treinamento.pc.pe.gov.br/#/controle" target="_blank" > SPP TREINAMENTO </a></li>
+            <li> <a href="http://200.238.83.219/old-PCPE/JAVA/" target="_blank" > JAVA </a></li>
+            <li> <a href="http://200.238.83.219/old-PCPE/AD/" target="_blank" > ANYDESK </a></li>
           </ul>
         </div>
         <Footer/>
